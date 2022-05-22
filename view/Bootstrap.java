1b9c50959b5caa59b7989b68821c4cbcb6831c53
GustavoFelixReal <gustavo.felixreal@gmail.com>
2022-05-22 19:49:45 -0300

    first commit

---
+++ view/Bootstrap.java
@@ -0,0 +1,17 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import scenes.InitScene;
+
+public class Bootstrap extends Application {
+  @Override
+  public void start(Stage stage) throws Exception {
+    stage.setScene(new InitScene().getScene());
+    stage.show();
+  }
+
+  public static void main(String[] args) {
+    Application.launch(Bootstrap.class, args);
+  }
+}
